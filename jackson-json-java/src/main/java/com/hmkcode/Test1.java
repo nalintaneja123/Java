923055079901a5e0e8f5dee92c2363d7b4876a1f
nalintaneja123 <nalintaneja123@gmail.com>
2017-01-16 00:11:49 +0530

    This is updating test1 of branch nalinbranch1

--- jackson-json-java/src/main/java/com/hmkcode/Test1.java
+++ jackson-json-java/src/main/java/com/hmkcode/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		int a=24;
 		System.out.println("This is"+a);
 		System.out.println("This is my Test1");
