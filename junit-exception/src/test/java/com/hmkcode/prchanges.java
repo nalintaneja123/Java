80901c01dcdace09066470cb66fd1bf6b10a431a
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-11 14:38:26 +0530

    my pr changes branch

--- junit-exception/src/test/java/com/hmkcode/prchanges.java
+++ junit-exception/src/test/java/com/hmkcode/prchanges.java
@@ -5,7 +5,11 @@ public class prchanges {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
 		System.out.println("sending u pr changes");
+
+		System.out.println("thiese are my pr changes");
+
 	}
 
 }
