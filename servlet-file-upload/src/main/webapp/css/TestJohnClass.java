da75f39fbe0ad6a42d34a2ed9c83bc7e51e95c4c
nalintaneja123 <nalintaneja123@gmail.com>
2017-02-15 21:48:43 +0530

    eer

--- servlet-file-upload/src/main/webapp/css/TestJohnClass.java
+++ servlet-file-upload/src/main/webapp/css/TestJohnClass.java
@@ -3,10 +3,8 @@ public class TestJohnClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		System.out.println("This is john travolta and dude u r doing well");
-
-
 
 	}
 
