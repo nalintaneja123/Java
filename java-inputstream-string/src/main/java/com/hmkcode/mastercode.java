7ee3b07d129a7612cc64396747a2d67867907d2d
nalintaneja123 <nalintaneja123@gmail.com>
2017-03-16 00:20:05 +0530

    Updated master code

--- java-inputstream-string/src/main/java/com/hmkcode/mastercode.java
+++ java-inputstream-string/src/main/java/com/hmkcode/mastercode.java
@@ -5,7 +5,7 @@ public class mastercode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("This is master code");
+		System.out.println("This is master code 22");
 	}
 
 }
